8d02a1dd9be5f1662cbd5c62a25c2ce224b3b67d
Khushmita Kaur <kkaur35@gatech.edu>
2024-07-30 13:26:55 -0700

    updated code

--- server.py
+++ server.py
@@ -10,15 +10,15 @@ class DisconnectionError(Exception): pass
 Error = namedtuple('Error', ('message',))
 
 # a class for parsing incoming requests and outgoing responses
-class ProtocolHandler(object):
+class ProtocolHandler:
     def handle_requests(self, socket_file):
         pass
     
     def write_response(self, socket_file, data):
         pass
     
-class Server(object):
-    def __init__(self, host='127.0.0.1', port=31337, max_clients=40):
+class Server:
+    def __init__(self, host='127.0.0.1', port=31337, max_clients=64):
         self.pool = Pool(max_clients)
         self.server = StreamServer((host, port), self.connection_handler, spawn=self.pool)
         
