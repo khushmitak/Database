de31cf6cc6848666d244f29df53a6015079924e1
Khushmita Kaur <kkaur35@gatech.edu>
2024-08-02 15:35:34 -0700

    implemented client class

--- server.py
+++ server.py
@@ -174,3 +174,17 @@ class Server:
         
     def run(self):
         self.server.serve_forever()
+        
+class Client:
+    def __init__(self, host='127.0.0.1', port='31337'):
+        self.protocol = ProtocolHandler()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((host, port))
+        self.fh = self.socket.makefile('rwb')
+
+    def execute(self, *args):
+        self.protocol.write_response(self.fh, args)
+        response = self.protocol.handle_request(self.fh)
+        if isinstance(response, Error):
+            raise CommandError(response.message)
+        return response
